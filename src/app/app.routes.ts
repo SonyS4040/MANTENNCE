591a175473095e574a6171e93599e6196233b877
SonyS4040 <103853684+SonyS4040@users.noreply.github.com>
2025-08-13 17:05:16 +0300

    Reverted all changes back to version 2ce77aefcde30369a19b0293092afad2abeedb1e

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 import { TicketsListComponent } from './tickets-list/tickets-list.component';
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './auth.guard';
 import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
-import { EngineersManagementComponent } from './engineers-management/engineers-management.component';
-import { AuthComponent } from './auth/auth.component';
-import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: CreateTicketComponent },
-  { path: 'login', component: AuthComponent },
-  { 
-    path: 'tickets', 
-    component: TicketsListComponent,
-    canActivate: [authGuard] 
-  },
-  { 
-    path: 'tickets/:id', 
-    component: TicketDetailComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'engineers',
-    component: EngineersManagementComponent,
-    canActivate: [authGuard]
-  },
-  { path: '**', redirectTo: '' }
+    { path: '', component: HomeComponent, title: 'الصفحة الرئيسية - أواسيس فارما' },
+    { path: 'create-ticket', component: CreateTicketComponent, title: 'إبلاغ عن عطل - أواسيس فارما' },
+    { 
+      path: 'tickets', 
+      component: TicketsListComponent, 
+      title: 'متابعة الأعطال - أواسيس فارما',
+      canActivate: [authGuard]
+    },
+    { 
+      path: 'tickets/:id', 
+      component: TicketDetailComponent, 
+      title: 'تفاصيل العطل - أواسيس فارما',
+      canActivate: [authGuard] 
+    },
+    { path: 'login', component: LoginComponent, title: 'تسجيل الدخول - أواسيس فارما' }
 ];
